cbfc6bcc6e90f32d2a0205f9f066d5c7cb163e50
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-01-12 22:24:40 +0530

    mock engine method

--- app/src/test/java/com/example/tddpractice/unittest/CarShould.kt
+++ app/src/test/java/com/example/tddpractice/unittest/CarShould.kt
@@ -5,17 +5,37 @@ import com.example.tddpractice.Engine
 import com.example.tddpractice.utils.MainCoroutineScopeRule
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 
 class CarShould {
 
     private val engine: Engine = mock()
-    private val car = Car(5.0, engine)
+    private val car: Car
+
+    init {
+        runBlocking {
+            whenever(engine.turnOn()).thenReturn(
+                flow {
+                    delay(2000)
+                    emit(25)
+                    delay(2000)
+                    emit(50)
+                    delay(2000)
+                    emit(95)
+                }
+            )
+        }
+        car = Car(5.0, engine)
+    }
 
     @ExperimentalCoroutinesApi
     @get:Rule
@@ -28,7 +48,7 @@ class CarShould {
     }
 
     @Test
-    fun turnOnItsEngine() = runBlocking {
+    fun turnOnItsEngine() = runBlockingTest {
         car.turnOn()
 
         verify(engine, times(1)).turnOn()
